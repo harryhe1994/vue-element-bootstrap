eb92abecbface3719d9c10379f533b1c60609b9b
harryhe1994 <harry.r.he@oocl.com>
2018-07-01 00:28:53 +0800

    init project

---
+++ vue-element-bootstrap/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <header-view/>
+    <div class='main'>
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderView from '@/components/HeaderView';
+
+export default {
+  name: 'App',
+  components: {
+    HeaderView,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    padding: 10px;
+    min-width: 1400px;
+  }
+  .main {
+    margin-top: 20px;
+    .router-view {
+      margin: auto;
+      font-size: 14px;
+    }
+  }
+</style>
---
+++ vue-element-bootstrap/src/components/HeaderView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header-view">
+    <span class='title' align='left'>Vue Admin</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderView',
+};
+</script>
+
+<style lang="scss" scoped>
+  .header-view {
+    background-color: steelblue;
+    width: 100%;
+    height: 120px;
+    .title {
+      font-size: 22px;
+      font-weight: 600;
+      line-height: 58px;
+      color: #ffffff;
+      float: left;
+      padding-left: 10px;
+      cursor: pointer;
+    }
+  }
+</style>
